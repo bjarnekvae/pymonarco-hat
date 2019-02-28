995ea1375d8cc7962dcef476f841260d68e168ff
Bjarne Kvæstad <bjarne.kvaestad@sintef.no>
2019-02-28 15:56:32 +0100

    update example

--- examples/monarco_example.py
+++ examples/monarco_example.py
@@ -3,15 +3,21 @@ import sys
 import time
 
 lib_path = sys.argv[1]
+plc_handler = plc.Monarco(lib_path, debug_flag=plc.MONARCO_DPF_WRITE | plc.MONARCO_DPF_VERB)
 
-print(lib_path)
-plc_object = plc.Monarco(lib_path, '/dev/spidev0.0', 4000000, "Monarco")
+plc_handler.set_pwm_frequency(plc.PWM_CHANNEL1, 1000)
+plc_handler.set_pwm_out(plc.DOUT2, 0.75)
+plc_handler.set_analog_out(plc.AOUT1, 5.0)
 
-while(1):
-    plc_object.set_digital_out(2, 1)
+while 1:
+    plc_handler.set_digital_out(plc.DOUT1, plc.HIGH)
     time.sleep(1)
-    plc_object.set_digital_out(2, 0)
+    plc_handler.set_digital_out(plc.DOUT1, plc.LOW)
     time.sleep(1)
+
+    print("DIN1:", plc_handler.get_digital_in(plc.DIN1))
+    print("AIN1:", plc_handler.get_analog_in(plc.AIN1))
+
 
 
 
