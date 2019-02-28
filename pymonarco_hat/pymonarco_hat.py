f477caa7a972fe26f15323b7f4c37001a8ed23c7
Bjarne Kvæstad <bjarne.kvaestad@sintef.no>
2019-02-28 14:21:28 +0100

    internal threading in module

--- pymonarco_hat/pymonarco_hat.py
+++ pymonarco_hat/pymonarco_hat.py
@@ -1,4 +1,6 @@
 import ctypes
+import threading
+import time
 
 
 class _monarco_struct_sdc_t(ctypes.Structure):
@@ -114,69 +116,86 @@ AIN1 = 1
 AIN2 = 2
 
 
-class Monarco:
-    def __init__(self, lib_path, spi_interface='/dev/spidev0.0', spi_clock=4000000, dprint_prefix='', debug_flag=0):
+class Monarco(threading.Thread):
+    def __init__(self, lib_path, spi_interface='/dev/spidev0.0', spi_clock=4000000,
+                 dprint_prefix='', debug_flag=0, cycle_interval=0.01):
+
         self.__monarco = ctypes.CDLL(lib_path)
         self.__cxt = _monarco_cxt_t()
         self.__monarco.set_dprint_flag(debug_flag)
         self.__monarco.monarco_init(ctypes.pointer(self.__cxt), ctypes.c_char_p(spi_interface.encode('utf-8')),
                                     ctypes.c_uint32(spi_clock), ctypes.c_char_p(dprint_prefix.encode('utf-8')))
 
+        threading.Thread.__init__(self)
+        self.cycle_interval = cycle_interval
+        self.__mutex = threading.Lock()
+        self.daemon = True
+        self.start()
+
+    def run(self):
+        while True:
+            with self.__mutex:
+                self.__monarco.monarco_main(ctypes.pointer(self.__cxt))
+            time.sleep(self.cycle_interval)
+
     def set_digital_out(self, port, value):
         assert port in [DOUT1, DOUT2, DOUT3, DOUT4], "Invalid digital out port"
         assert value == LOW or value == HIGH, "Invalid digital out value"
 
-        if value == LOW:
-            self.__cxt.tx_data.dout = ctypes.c_uint8(self.__cxt.tx_data.dout & ~(1 << port-1))
-        elif value == HIGH:
-            self.__cxt.tx_data.dout = ctypes.c_uint8(self.__cxt.tx_data.dout | (1 << port-1))
+        with self.__mutex:
+            if value == LOW:
+                self.__cxt.tx_data.dout = ctypes.c_uint8(self.__cxt.tx_data.dout & ~(1 << port-1))
+            elif value == HIGH:
+                self.__cxt.tx_data.dout = ctypes.c_uint8(self.__cxt.tx_data.dout | (1 << port-1))
 
     def get_digital_in(self, port):
         assert port in [DIN1, DIN2, DIN3, DIN4], "Invalid digital in port"
-        return self.__cxt.rx_data.din & (1 << port-1)
+
+        with self.__mutex:
+            return self.__cxt.rx_data.din & (1 << port-1)
 
     def set_pwm_frequency(self, channel,  pwm_frequency):
         assert channel in [PWM_CHANNEL1, PWM_CHANNEL2], "Invalid PWM port"
         assert 1.0 <= pwm_frequency < 100000.0, "PWM frequency out of range: {}".format(pwm_frequency)
 
-        if channel == PWM_CHANNEL1:  # DOUT 1, 2, 3
-            self.__cxt.tx_data.pwm1_div = self.__monarco.monarco_util_pwm_freq_to_u16(ctypes.c_double(pwm_frequency))
-        elif channel == PWM_CHANNEL2:  # DOUT 4
-            self.__cxt.tx_data.pwm2_div = self.__monarco.monarco_util_pwm_freq_to_u16(ctypes.c_double(pwm_frequency))
+        with self.__mutex:
+            if channel == PWM_CHANNEL1:  # DOUT 1, 2, 3
+                self.__cxt.tx_data.pwm1_div = self.__monarco.monarco_util_pwm_freq_to_u16(ctypes.c_double(pwm_frequency))
+            elif channel == PWM_CHANNEL2:  # DOUT 4
+                self.__cxt.tx_data.pwm2_div = self.__monarco.monarco_util_pwm_freq_to_u16(ctypes.c_double(pwm_frequency))
 
     def set_pwm_out(self, port, value):
         assert port in [DOUT1, DOUT2, DOUT3, DOUT4], "Invalid digital out port"
         assert 0 <= value <= 1, "PWM value out of range: {}".format(value)
 
-        if port == DOUT1:
-            self.__cxt.tx_data.pwm1a_dc = self.__monarco.monarco_util_pwm_dc_to_u16(ctypes.c_double(value))
-        elif port == DOUT2:
-            self.__cxt.tx_data.pwm1b_dc = self.__monarco.monarco_util_pwm_dc_to_u16(ctypes.c_double(value))
-        elif port == DOUT3:
-            self.__cxt.tx_data.pwm1c_dc = self.__monarco.monarco_util_pwm_dc_to_u16(ctypes.c_double(value))
-        elif port == DOUT4:
-            self.__cxt.tx_data.pwm2a_dc = self.__monarco.monarco_util_pwm_dc_to_u16(ctypes.c_double(value))
+        with self.__mutex:
+            if port == DOUT1:
+                self.__cxt.tx_data.pwm1a_dc = self.__monarco.monarco_util_pwm_dc_to_u16(ctypes.c_double(value))
+            elif port == DOUT2:
+                self.__cxt.tx_data.pwm1b_dc = self.__monarco.monarco_util_pwm_dc_to_u16(ctypes.c_double(value))
+            elif port == DOUT3:
+                self.__cxt.tx_data.pwm1c_dc = self.__monarco.monarco_util_pwm_dc_to_u16(ctypes.c_double(value))
+            elif port == DOUT4:
+                self.__cxt.tx_data.pwm2a_dc = self.__monarco.monarco_util_pwm_dc_to_u16(ctypes.c_double(value))
 
     def set_analog_out(self, port, value):
         assert port in [AOUT1, AOUT2], "Invalid analog out port"
         assert 0 <= value <= 10, "PWM value out of range: {}".format(value)
 
-        if port == AOUT1:
-            self.__cxt.tx_data.aout1 = self.__monarco.monarco_util_aout_volts_to_u16(ctypes.c_double(value))
-        elif port == AOUT2:
-            self.__cxt.tx_data.aout2 = self.__monarco.monarco_util_aout_volts_to_u16(ctypes.c_double(value))
+        with self.__mutex:
+            if port == AOUT1:
+                self.__cxt.tx_data.aout1 = self.__monarco.monarco_util_aout_volts_to_u16(ctypes.c_double(value))
+            elif port == AOUT2:
+                self.__cxt.tx_data.aout2 = self.__monarco.monarco_util_aout_volts_to_u16(ctypes.c_double(value))
 
     def get_analog_in(self, port):
         assert port in [AIN1, AIN2], "Invalid analog in port"
-        if port == AIN1:
-            return self.__cxt.rx_data.ain1 * 10.0/4095.0
-        elif port == AIN2:
-            return self.__cxt.rx_data.ain2 * 10.0/4095.0
 
-    def update(self):
-        self.__monarco.monarco_main(ctypes.pointer(self.__cxt))
+        with self.__mutex:
+            if port == AIN1:
+                return self.__cxt.rx_data.ain1 * 10.0/4095.0
+            elif port == AIN2:
+                return self.__cxt.rx_data.ain2 * 10.0/4095.0
 
     def close(self):
         self.__monarco.monarco_exit(ctypes.pointer(self.__cxt))
-
-
